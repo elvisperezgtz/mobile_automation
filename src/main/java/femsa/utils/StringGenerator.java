bdca024a7a6002ad8734e1283ef7455502b14e71
Elvis PErez <elvisperezgtz@gmail.com>
2023-02-01 18:46:28 -0500

    se agrega metodo para generar strings de numeros

--- src/main/java/femsa/utils/StringGenerator.java
+++ src/main/java/femsa/utils/StringGenerator.java
@@ -2,6 +2,9 @@ package femsa.utils;
 
 public class StringGenerator {
 
+    private StringGenerator(){
+
+    }
     public static String generateRandomString(int length, boolean specialChars, boolean numbers) {
         String allowedChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
         if (specialChars) {
@@ -18,9 +21,15 @@ public class StringGenerator {
         return randomString;
     }
 
-    public static void main(String[] args) {
-        System.out.println(generateRandomString(32,false,false));
-        System.out.println(generateRandomString(32,false,false));
-        System.out.println(generateRandomString(257,true,false));
+    public static String withOnlyRandomNumbers(int length) {
+        String allowedChars = "0123456789";
+
+        String randomString = "";
+        for (int i = 0; i < length; i++) {
+            int randomIndex = (int) (Math.random() * allowedChars.length());
+            randomString += allowedChars.charAt(randomIndex);
+        }
+        return randomString;
     }
+
 }
