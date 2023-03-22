8f34a449ec2b8e69d4efdcef484beb42ff6d96dd
Elvis PErez <elvisperezgtz@gmail.com>
2023-03-21 20:40:17 -0500

    changed tag

--- src/test/java/femsa/CucumberTestSuite.java
+++ src/test/java/femsa/CucumberTestSuite.java
@@ -8,6 +8,6 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         plugin = {"pretty"},
         features = "src/test/resources/features",
-        tags = "@clabe"
+        tags = "@login"
 )
 public class CucumberTestSuite {}
