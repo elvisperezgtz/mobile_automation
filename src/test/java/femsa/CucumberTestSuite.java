80fe766f39135cf76de44834b7c32b0daa03be42
Elvis PErez <elvisperezgtz@gmail.com>
2023-04-27 18:54:04 -0500

    modified regression tag

--- src/test/java/femsa/CucumberTestSuite.java
+++ src/test/java/femsa/CucumberTestSuite.java
@@ -8,6 +8,6 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         plugin = {"pretty"},
         features = "src/test/resources/features",
-        tags = "@onboarding"
+        tags = "@regression"
 )
 public class CucumberTestSuite {}
