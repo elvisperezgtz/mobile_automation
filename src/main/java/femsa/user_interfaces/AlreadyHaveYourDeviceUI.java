50a84bbe9237da5fc4682bbb55557a4585c16ada
LuisArcia <larcia@paloi-it.com>
2023-07-19 16:47:49 -0500

    Add xpath for onboarding UI

--- src/main/java/femsa/user_interfaces/AlreadyHaveYourDeviceUI.java
+++ src/main/java/femsa/user_interfaces/AlreadyHaveYourDeviceUI.java
@@ -6,11 +6,11 @@ import org.openqa.selenium.By;
 public class AlreadyHaveYourDeviceUI {
     public static final Target TITLE = Target.the("Title of the Already have your device? screen")
             .locatedForAndroid(By.xpath("//android.widget.TextView[@content-desc=\"TEXT_TITLE\"]"))
-            .locatedForIOS(By.xpath(""));
+            .locatedForIOS(By.xpath("//XCUIElementTypeStaticText[@name=\"INTROMPOS_TITLE_LABEL\"]"));
     public static final Target I_HAVE_IT = Target.the("I have it button of the Already have your device? screen")
             .locatedForAndroid(By.xpath("//android.widget.TextView[@text='Ya lo tengo']"))
-            .locatedForIOS(By.xpath(""));
+            .locatedForIOS(By.xpath("//XCUIElementTypeButton[@name=\"INTROMPOS_HAVEDEVICE_BTN\"]"));
     public static final Target I_DONT_HAVE = Target.the("I don't have it yet button of the Already have your device? screen")
             .locatedForAndroid(By.xpath(""))
-            .locatedForIOS(By.xpath(""));
+            .locatedForIOS(By.xpath("//XCUIElementTypeButton[@name=\"INTROMPOS_NOTHAVEDEVICE_BTN\"]"));
 }
