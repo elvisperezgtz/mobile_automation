ed4f2d64ddf6dbe65dd3f85ce4ba991d315d4dc3
Elvis PErez <elvisperezgtz@gmail.com>
2023-08-07 19:09:24 -0500

    added new method

--- src/main/java/femsa/utils/PhoneNumberGenerator.java
+++ src/main/java/femsa/utils/PhoneNumberGenerator.java
@@ -30,4 +30,25 @@ public class PhoneNumberGenerator {
         return -1;
     }
 
+    public static void main(String[] args) throws IOException {
+        System.out.println("Last phone number  ====== " + getLastPhoneNumber("src/test/resources/data/phone_numbers/luis_phone_numbers.txt"));
+        long generateNewNumber = generateNumbers("src/test/resources/data/phone_numbers/luis_phone_numbers.txt");
+
+        System.out.println("New number generate and stored in the phone numbers onboading file " + generateNewNumber);
+    }
+
+    public static String getLastPhoneNumber(String filePath) throws RuntimeException {
+        long lastPhoneNumber = 0;
+        try {
+            Path path = Paths.get(filePath);
+            List<String> lines = Files.readAllLines(path);
+
+            if (!lines.isEmpty()) {
+                lastPhoneNumber = Long.parseLong(lines.get(lines.size() - 1));
+            }
+            return String.valueOf(lastPhoneNumber);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
