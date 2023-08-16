c6ba51d46ab0f9d27758a3431b41041453791f17
Elvis PErez <elvisperezgtz@gmail.com>
2023-08-16 12:54:59 -0500

    feat: :sparkles: modified Save button xpath

--- src/main/java/femsa/user_interfaces/CommonsUI.java
+++ src/main/java/femsa/user_interfaces/CommonsUI.java
@@ -50,7 +50,7 @@ public class CommonsUI {
             .locatedForIOS(xpath("//XCUIElementTypeButton[@name=\"backWhite\"]"));
 
     public static final Target SAVE = Target.the("Save button")
-            .locatedForAndroid(By.xpath("//android.widget.TextView[@text='Guardar' or @text='GUARDAR']"))
+            .locatedForAndroid(By.xpath("//android.view.View[@content-desc=\"BUTTON_SAVE\"]"))
             .locatedForIOS(By.name("PROFILE_PERSONALDATA_SAVE_BTN"));
 
     public static final Target CONTINUE = Target.the("Continue button")
