99c39b325f05a77c4cb3a40634a9f05c12e59f52
LuisArcia <larcia@paloi-it.com>
2023-08-30 07:56:11 -0500

    Add xpath for RegisterInThreeStepsUI iOS

--- src/main/java/femsa/user_interfaces/RegisterInThreeStepsUI.java
+++ src/main/java/femsa/user_interfaces/RegisterInThreeStepsUI.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.By;
 public class RegisterInThreeStepsUI {
     public static final Target BEGIN_REGISTRATION = Target.the("begin registration button")
             .locatedForAndroid(By.xpath("//android.widget.TextView[@text='Empezar mi registro']"))
-            .locatedForIOS(By.xpath("//XCUIElementTypeStaticText[@name='Empezar mi registro   ']"));
+            .locatedForIOS(By.xpath("//XCUIElementTypeButton[@name=\"TUTORIAL_REGISTER_BTN\"]"));
 
     public static final Target ALREADY_HAVE_ACCOUNT = Target.the("I already have an account button")
             .locatedForAndroid(By.xpath("//android.widget.TextView[@text = 'Ya tengo cuenta']"))
